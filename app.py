489e55c614be0e3ea01a7173efe256f8fa48c241
Domingo Fraga Hernandez <dfraga547@gmail.com>
2026-04-23 11:39:10 -0500

    Encapsulate database connection within route functions
    Moved database connection inside route functions for better encapsulation.

--- app.py
+++ app.py
@@ -5,23 +5,29 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-import mysql.connector
-mydb = mysql.connector.connect(
-    host="46.28.42.226",
-    user="u760464709_prueba_usr",
-    password="F7Ldev4h~l",
-    database="u760464709_prueba_bd"
-)
-
 @app.route('/productos')
 def buscarProductos():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_prueba_usr",
+        password="F7Ldev4h~l",
+        database="u760464709_prueba_bd"
+    )
     mycursor = mydb.cursor()
     mycursor.execute("SELECT * FROM productos")
     myresult = mycursor.fetchall()
     return make_response(jsonify(myresult))
 
 @app.post('/producto')
-def agregarProducto():
+    def agregarProducto():
+    import mysql.connector
+    mydb = mysql.connector.connect(
+        host="46.28.42.226",
+        user="u760464709_prueba_usr",
+        password="F7Ldev4h~l",
+        database="u760464709_prueba_bd"
+    )
     mycursor = mydb.cursor()
     sql = "INSERT INTO productos (nombre, categoria, precio, existencias) VALUES (%s, %s, %s, %s)"
     val = (request.form['txtNombre'], request.form['cboCategoria'], request.form['txtPrecio'], request.form['txtExistencias'])
